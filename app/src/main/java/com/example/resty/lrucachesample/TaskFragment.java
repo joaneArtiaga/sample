4fa77736de00f79b7c4b846affef672147d8aa7a
Joane Therese Artiaga <Joane Therese Artiaga>
2016-12-16 05:17:46 +0800

    Third

---
+++ app/src/main/java/com/example/resty/lrucachesample/TaskFragment.java
@@ -0,0 +1,13 @@
+package com.example.resty.lrucachesample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_fragment);
+    }
+}
