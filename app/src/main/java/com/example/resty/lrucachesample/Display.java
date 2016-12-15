25df69518a29af59eaaa368619f00232be84f5bd
Joane Therese Artiaga <Joane Therese Artiaga>
2016-12-16 06:41:34 +0800

    Third commit

--- app/src/main/java/com/example/resty/lrucachesample/Display.java
+++ app/src/main/java/com/example/resty/lrucachesample/Display.java
@@ -3,11 +3,11 @@ package com.example.resty.lrucachesample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TaskFragment extends AppCompatActivity {
+public class Display extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_fragment);
+        setContentView(R.layout.activity_display);
     }
 }
